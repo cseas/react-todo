2a1b8b6b209f046cdd098da22bb1eebc5adc4ca0
abhijeetsinghtomar <abhijeetsinghemail@gmail.com>
2019-12-24 14:56:06 +0530

    Updates

---
+++ precache-manifest.a1080506577b19eeedd737d4378e0a90.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "080caeaa728baafe8dc8a501f52bd7ab",
+    "url": "/react-todo/index.html"
+  },
+  {
+    "revision": "9147a1cd25a362b9eeb3",
+    "url": "/react-todo/static/css/main.f9be48e5.chunk.css"
+  },
+  {
+    "revision": "59f6b58a777cf6c01585",
+    "url": "/react-todo/static/js/2.3565f9e4.chunk.js"
+  },
+  {
+    "revision": "d705cb622423d72c5defbf368ca70dcc",
+    "url": "/react-todo/static/js/2.3565f9e4.chunk.js.LICENSE"
+  },
+  {
+    "revision": "9147a1cd25a362b9eeb3",
+    "url": "/react-todo/static/js/main.44594180.chunk.js"
+  },
+  {
+    "revision": "84a1915f5341ab2a40a5",
+    "url": "/react-todo/static/js/runtime-main.91b7fbf6.js"
+  }
+]);
