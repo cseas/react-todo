dd6841f40a0f8bea6ef7b0665b270817cc1eeff8
abhijeetsinghtomar <abhijeetsinghemail@gmail.com>
2019-12-24 15:05:23 +0530

    Add check for completed tasks

--- src/TodoItem.js
+++ src/TodoItem.js
@@ -2,10 +2,11 @@ import React from "react";
 
 function TodoItem(props) {
   return (
-      <div className="todo-item">
-        <input type="checkbox" />
-  <p>{props.todo.text}</p>
-      </div>
+    <div className="todo-item">
+      <input type="checkbox" 
+        checked={props.todo.completed} />
+      <p>{props.todo.text}</p>
+    </div>
   );
 }
 
