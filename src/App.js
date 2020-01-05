084377d4264254b4b732ba08f36e7dd636e5f670
abhijeetsinghtomar <abhijeetsinghemail@gmail.com>
2020-01-05 16:33:19 +0530

    Add state

--- src/App.js
+++ src/App.js
@@ -3,16 +3,25 @@ import TodoItem from "./TodoItem";
 import todoData from "./todoData";
 import "./style.css";
 
-function App() {
-  const todoComponents = todoData.map(todo => 
-    <TodoItem key={todo.id}
-      todo={todo} />)
-  
-  return (
-    <div className="todo-list">
-      {todoComponents}
-    </div>
-  );
+class App extends React.Component {
+  constructor() {
+    super();
+    this.state = {
+      todos: todoData
+    }
+  }
+
+  render() {
+    const todoComponents = this.state.todos.map(todo => 
+      <TodoItem key={todo.id}
+        todo={todo} />)
+    
+    return (
+      <div className="todo-list">
+        {todoComponents}
+      </div>
+    );
+  }
 }
 
 export default App;
